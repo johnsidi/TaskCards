bd6ac879e3718cf156af5e49b5096c95714a7c12
John Sidiropoulos <contact@johnsidi.com>
2021-11-04 09:14:36 +0000

    trying to fix env

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./router');
 const connection = require('./models/index');
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
 const localPort = 3001;
