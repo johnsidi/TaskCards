d2e98d721a60d80bf1738662abd12aabc7007c92
John Sidiropoulos <contact@johnsidi.com>
2021-11-23 08:32:01 +0000

    fix: ready for deployment

--- server/models/index.js
+++ server/models/index.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
 
 //for local development
-module.exports = mongoose.connect('mongodb://localhost:27017/tasks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+// module.exports = mongoose.connect('mongodb://localhost:27017/tasks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+// });
 
 // for deployment
 
-// module.exports = mongoose.connect(
-//   'mongodb+srv://' +
-//     process.env.MongoDBuser +
-//     ':' +
-//     process.env.MongoDBpass +
-//     '@' +
-//     process.env.DBcluster +
-//     '.vpxhl.mongodb.net/' +
-//     process.env.DBname +
-//     '?retryWrites=true&w=majority',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//   }
-// );
+module.exports = mongoose.connect(
+  'mongodb+srv://' +
+    process.env.MongoDBuser +
+    ':' +
+    process.env.MongoDBpass +
+    '@' +
+    process.env.DBcluster +
+    '.vpxhl.mongodb.net/' +
+    process.env.DBname +
+    '?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
